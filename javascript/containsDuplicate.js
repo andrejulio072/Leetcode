b3238bddb0464ae35a8de1f6fab9b6e6ba550677
Andre Garcia <andrejulio072@hotmail.com>
2022-09-15 12:22:17 +0100

    leet code 100 days

---
+++ javascript/containsDuplicate.js
@@ -0,0 +1,11 @@
+var containsDuplicate = function(nums) {
+  let set = new Set()
+  for(let i=0;i<nums.length;i++){
+      let item = nums[i];
+      if(set.has(item) ){
+          return true;
+      }
+      set.add(item);
+  }
+  return false;
+};
