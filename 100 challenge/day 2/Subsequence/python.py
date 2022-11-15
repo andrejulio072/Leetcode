80726aaf69078d72edf462c340cec6d18a3a4bf4
Andre Garcia <andrejulio072@hotmail.com>
2022-11-15 02:26:30 +0000

    100 challenge

--- 100 challenge/day 2/Subsequence/python.py
+++ 100 challenge/day 2/Subsequence/python.py
@@ -10,3 +10,20 @@ class Solution:
 
                     subsequence+=1
         return  subsequence == len(s)
+
+
+ def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i=j=c = 0
+        while i<len(s)and j<len(t):
+            if (s[i]==t[j]):
+                i+=1
+                j+=1
+                c+=1
+            else:
+                j+=1
+        return True if c==len(s) else False
