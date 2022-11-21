1b3d9d74b83464c7971c26d8b112e05dbfd2850f
Andre Garcia <andrejulio072@hotmail.com>
2022-11-21 23:34:11 +0000

    day 4 coding skills

--- 100 challenge/day 4/hammer weight/py.py
+++ 100 challenge/day 4/hammer weight/py.py
@@ -1,3 +1,32 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         return bin(n).count('1')
+
+
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+# 1st method
+        c = 0
+        b = bin(n)[2:]
+        for i in b:
+            if i == '1':
+                c = c + 1
+        return c
+
+# 2nd method
+
+        res = 0
+        for i in range(32):
+            res += n & 1
+            n = n >> 1
+
+        return res
+
+# 3rd method
+
+        res = 0
+        while n:
+            n = n & (n-1)
+            res += 1
+
+        return res
