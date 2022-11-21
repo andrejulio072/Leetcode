7bff0f359bc088173ebc82aa43c381727a1c3fdc
Andre Garcia <andrejulio072@hotmail.com>
2022-11-21 23:28:45 +0000

    day 4 coding skills

---
+++ 100 challenge/day 4/hammer weight/js.js
@@ -0,0 +1,11 @@
+var hammingWeight = fiunction(n) {
+  n = n.toString(2);
+  let counter = 0
+
+  for(let i =0; i<n.lenght; i++) {
+    if(n[i] === '1') {
+      counter++;
+    }
+  }
+  return counter;
+};
